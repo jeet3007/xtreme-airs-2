c99d6964536fdedb0b89a8eef67e153caa427c4d
jeet3007 <jeet.thakkar8@gmail.com>
2016-01-12 21:35:11 +0530

    all inn

---
+++ _site/js/custom.js
@@ -0,0 +1,48 @@
+(function($) {
+    'use strict';
+
+    /* Hide menu after click
+    ----------------------------------------------*/
+    $('.navbar-nav li a').click(function(event) {
+        $('.in').collapse('hide');
+    });
+
+    /* Smooth scroll to section
+    ----------------------------------------------*/
+    $('a.scroll[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top-70
+                }, 500);
+                return false;
+            }
+        }
+    });
+
+    /* Team slideshow
+    ----------------------------------------------*/
+    $("#team-carousel").owlCarousel({
+        autoPlay: 5000, //Set AutoPlay to 5 seconds
+        items : 3,
+        itemsDesktopSmall : [979,3],
+        stopOnHover: true
+
+    });
+  /* Tooltip
+    ----------------------------------------------*/
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* Lightbox
+    ----------------------------------------------*/
+    $('.image-link').magnificPopup({
+        type:'image'
+    });
+
+    /* Google map
+    ----------------------------------------------*/
+
+})(jQuery);
